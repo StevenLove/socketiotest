ce302b91d39a2a3ed5ff7c30f476d3f0bc4249e3
Steven Love <lovestevend@gmail.com>
2017-01-20 13:23:42 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+DESCRIPTION
+This is a test project working with socket.io
+This is based off of the tutorial chat program from http://socket.io/get-started/chat/ with some refactoring
+
+BUILDING
+>>npm install
+>>node index.js
---
+++ index.js
@@ -0,0 +1,33 @@
+// This is a test application to investigate usage of socket.io
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var indexFilename = 'index.html';
+var indexAbsPath = __dirname + '/' + indexFilename;
+
+app.get('/', function(req, res){
+  res.sendFile(indexAbsPath);
+});
+
+var clientjsFilename = 'client.js';
+var clientjsAbsPath = __dirname + "/" + clientjsFilename;
+
+app.get('/client.js', function(req,res){
+  res.sendFile(clientjsAbsPath);
+})
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
