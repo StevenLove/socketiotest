c593d06207eba7b71050cc58c80703bc16e8a5bd
Steven Love <lovestevend@gmail.com>
2017-01-20 15:05:14 -0500

    can move shapes around locally, basic fabricjs functionality

--- client.js
+++ client.js
@@ -7,3 +7,19 @@ $('form').submit(function(){
 socket.on('chat message', function(msg){
   $('#messages').append($('<li>').text(msg));
 });
+
+
+
+
+// fabric
+var canvas = new fabric.Canvas('c');
+// canvas.isDrawingMode=true;
+
+var circle = new fabric.Circle({
+  radius: 20, fill: 'green', left: 100, top: 100
+});
+var triangle = new fabric.Triangle({
+  width: 20, height: 30, fill: 'blue', left: 50, top: 50
+});
+
+canvas.add(circle, triangle);
